0f9e9779c8003f67fb6baaa48c9e63b848146a64
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-08-05 00:42:40 +0300

    add render port

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST, PORT = 5000 } = process.env;
+// const { DB_HOST, PORT = 5000 } = process.env;
 
+const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    console.log("Database connection successful");
-    app.listen(PORT, () =>
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    );
-  })
+    .connect(DB_HOST)
+    .then(() => {
+        console.log("Database connection successful");
+        app.listen(PORT, () =>
+            console.log(`Server running. Use our API on port: ${PORT}`)
+        );
+    })
 
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+    .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+    });
