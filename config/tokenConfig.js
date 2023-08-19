bd438b57a20b39b5271e063f34f3a146f646e0f2
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-08-19 16:27:30 +0300

    change expiresIn

--- config/tokenConfig.js
+++ config/tokenConfig.js
@@ -3,11 +3,11 @@ module.exports = {
         tokens: {
             access: {
                 type: "access",
-                expiresIn: "7d",
+                expiresIn: "3m",
             },
             refresh: {
                 type: "refresh",
-                expiresIn: "90d",
+                expiresIn: "7d",
             },
         },
     },
